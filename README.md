d289918feb4df19e1b1bd14be11e54cca9ad4485
MarcIsCoding <marcdgarside@gmail.com>
2020-12-08 17:33:21 +0100

    Complete v1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Progress Steps With Changing Card
+
+Simple changing card with progress bar. Can be used for forms, info cards or anything else....
+
+## Technologies Used
+
+Pure HTML, CSS and vanilla JavaScript. That's it...
---
+++ script.js
@@ -0,0 +1,72 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+const titles = document.querySelectorAll('.card-title')
+
+let currentActive = 1
+let currentShow = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+    currentShow++
+    if(currentActive > circles.length){
+        currentActive = circles.length
+    }
+    if(currentShow > titles.length){
+        currentShow = titles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+    if(currentActive < 1){
+        currentActive = 1
+    }
+    currentShow--
+    if(currentShow < 1){
+        currentShow = 1
+    }
+
+    update()
+})
+
+function update(){
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    })
+    titles.forEach((title, idx) => {
+        if(idx < currentShow){
+            removeShowClasses()
+            title.classList.add('show')
+        }else{
+            title.classList.remove('show')
+        }
+    })
+
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length -1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    }else if (currentActive === circles.length){
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
+
+function removeShowClasses() {
+    titles.forEach(title => {
+        title.classList.remove('show')
+    })
+}
